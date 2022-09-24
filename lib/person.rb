ebfeb9fbb53ae0021da4fbc83a5741f4d158dc17
sheready-stl <stella.waithera@moringaschool.com>
2022-09-24 19:15:45 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   # your code here
+  def initialize(attribute)
+    attribute.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
